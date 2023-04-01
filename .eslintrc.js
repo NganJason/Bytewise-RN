2da8d7a96ff1d2b310ef5f004f3660ca6a9e0f3d
Jonathan Seow <jonathan.seowjn@shopee.com>
2023-04-01 23:04:07 +0800

    Add lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,24 @@ module.exports = {
   extends: '@react-native-community',
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'react-native',
+            importNames: ['SafeAreaView'],
+            message:
+              'Import SafeAreaView from react-native-safe-area-context instead',
+          },
+          {
+            name: 'react-native-safe-area-view',
+            importNames: ['SafeAreaView'],
+            message:
+              'Import SafeAreaView from react-native-safe-area-context instead',
+          },
+        ],
+      },
+    ],
   },
 };
