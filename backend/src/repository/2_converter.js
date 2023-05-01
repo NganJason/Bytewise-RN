1fbdb38a56cf83e8db7defd4066207233ee7065f
Jonathan Seow <jonathan.seowjn@shopee.com>
2023-05-01 23:46:05 +0800

    Minor changes

--- backend/src/repository/2_converter.js
+++ backend/src/repository/2_converter.js
@@ -2,7 +2,6 @@ import { newCategory } from '../domain/entity/category';
 
 export const toCategoryPo = categoryEntity => {
   return {
-    id: categoryEntity.cat_id,
     cat_name: categoryEntity.cat_name,
     cat_type: categoryEntity.cat_type,
   };
