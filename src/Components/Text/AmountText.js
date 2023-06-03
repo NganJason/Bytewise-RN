3e5380b0b2d09f89903a4aaee3bc3872cc1657ba
Jonathan Seow <jonathan.seowjn@shopee.com>
2023-06-03 08:05:08 +0800

    use number for amount

--- src/Components/Text/AmountText.js
+++ src/Components/Text/AmountText.js
@@ -16,9 +16,7 @@ const AmountText = ({
   const { theme } = useTheme();
   const styles = getStyles(theme);
 
-  let amount = Number(children);
-
-  const getAmountAttr = () => {
+  const getAmountAttr = amount => {
     if (amount > 0) {
       return { styles: showColor && styles.positive, symbol: '+' };
     }
@@ -33,13 +31,13 @@ const AmountText = ({
   const renderAmountText = () => {
     let text = '';
 
-    const { symbol } = getAmountAttr();
+    const { symbol } = getAmountAttr(children);
 
     // use symbol string to show negative
-    if (amount < 0) {
-      amount = amount * -1;
+    if (children < 0) {
+      children = children * -1;
     }
-    amount = amount.toFixed(2);
+    const amount = children.toFixed(2);
 
     // add currency
     text = `${CURRENCY} ${amount}`;
@@ -55,7 +53,7 @@ const AmountText = ({
   return (
     <BaseText
       center={center}
-      style={{ ...getAmountAttr()?.styles, ...style }}
+      style={{ ...getAmountAttr(children)?.styles, ...style }}
       numberOfLines={1}
       {...props}>
       {renderAmountText()}
